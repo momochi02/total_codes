8a4e203ceac08a26062e65c498d376ec69f1e309
momochi02 <sel.19527120@gmail.com>
2025-07-16 18:00:20 +0700

    update

--- test_fontend/xml-compare-ui/src/utils/fetchWithRetry.js
+++ test_fontend/xml-compare-ui/src/utils/fetchWithRetry.js
@@ -1,13 +1,8 @@
-// utils/fetchWithRetry.js
-import axios from "axios";
-export async function fetchWithRetry(url, options = {}, maxRetries = 3, delay = 1000) {
+export async function fetchWithRetry(url, data = {}, config = {}, maxRetries = 3, delay = 1000) {
   let attempt = 0;
   while (attempt < maxRetries) {
     try {
-      const response = await fetch(url, options);
-      if (!response.ok) {
-        throw new Error(`Server trả về lỗi ${response.status}`);
-      }
+      const response = await axios.post(url, data, config);
       return response;
     } catch (error) {
       attempt++;
@@ -15,7 +10,7 @@ export async function fetchWithRetry(url, options = {}, maxRetries = 3, delay = 
       if (attempt >= maxRetries) {
         throw new Error("❌ Gửi request thất bại sau nhiều lần thử.");
       }
-      await new Promise((res) => setTimeout(res, delay)); // đợi delay ms
+      await new Promise((res) => setTimeout(res, delay));
     }
   }
 }
